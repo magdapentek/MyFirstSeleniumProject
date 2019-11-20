958c5cd51a9fb19040ed329e81b829d3f6257c7f
magdapentek <55142956+magdapentek@users.noreply.github.com>
2019-11-20 08:01:06 +0200

    Update RegisterTest.java

--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -15,7 +15,7 @@ public class RegisterTest {
         driver.findElement(By.cssSelector("#header-account > div > ul > li:nth-child(5) > a")).click();
         driver.findElement(By.id("firstname")).sendKeys("Pentek");
         driver.findElement(By.name("lastname")).sendKeys("Magda");
-        driver.findElement(By.id("email_address")).sendKeys("magdapenteka@yahoo.com");
+        driver.findElement(By.id("email_address")).sendKeys("magdipenteka30@yahoo.com");
         driver.findElement(By.name("password")).sendKeys("0246810");
         driver.findElement(By.id("confirmation")).sendKeys("0246810");
         driver.findElement(By.id("is_subscribed")).click();
